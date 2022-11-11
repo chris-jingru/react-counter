fa79203caad48bc3918dcc140f4a54eb48abed00
ChristineWest <christinewwwest@gmail.com>
2022-11-11 16:24:22 -0500

    added counter parts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>this is a counter</header>
+      <section className="counter">
+        <button className="counterBtnUp">+</button>
+        <div className="numberDisplay">0</div>
+
+        <button className="counterBtnDown">-</button>
+      </section>
+      <footer>made by JX and CW</footer>
+    </>
   );
 }
 
